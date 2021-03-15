d1c2fcbf18559ee7f4ee4d571523e2d5505af725
plubzzzz <bastien.grosclaude@orange.fr>
2021-03-15 17:04:14 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,50 @@
-document.querySelector('#start').addEventListener('click',function (e){
+let p = null
 
-    navigator.getUserMedia({
-        video: true,
-        audio: true,
-    },function (stream) {
-        let emitterVideo = document.querySelector('#emitter-video')
-        emitterVideo.src = window.URL.createObjectURL(stream)
-        emitterVideo.play()
-    }, function () {})
+function bindEvents(p){
+  p.on('error', function (err){
+      console.log('error', err )
+  })
+
+p.on('signal',function (data){
+document.querySelector('#offer').textContent = JSON.stringify(data)
+})
+/*
+p.on('stream', function(stream){
+let video = document.querySelector('#receiver-video')
+video.srcObject= stream;
+  video.play();
+*/
+}
+
+
+document.querySelector('#start').addEventListener('click', function (e){
+   navigator.mediaDevices.getUserMedia({audio: true, video: true})
+   .then((stream) => {
+     let  p = new SimplePeer({
+           initiator: true,
+           stream: stream,
+           trickle: false
+       })
+       bindEvents(p)
+       let emitterVideo = document.getElementById('emitter-video')
+       emitterVideo.srcObject = stream;
+       emitterVideo.play()
+     })
+     .catch((error) => {
+       console.log('UHOH!', error)
+     })
+})
+
+document.querySelector('#incoming').addEventListener('submit', function(e){
+  e.preventDefault()
+if (p == null) {
+    p = new SimplePeer({
+       initiator: false,
+       trickle : false 
+
+   })
+   bindEvents(p)
+ }
+ p.signal(JSON.parse(e.target.querySelector('textarea').value))
 
 })
